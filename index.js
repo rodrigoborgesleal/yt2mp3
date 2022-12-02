0a66191a20c9a30e8b5bfaf950f99207e862b998
rodrigoborgesleal <study.borges@gmail.com>
2022-12-01 22:16:03 -0300

    Deploy

---
+++ index.js
@@ -0,0 +1,46 @@
+const fs = require('fs')
+const { downloadSong } = require('ytdl-mp3')
+const ytdl = require('ytdl-core')
+const readlineSync = require('readline-sync')
+
+
+let urlVids = []
+
+
+if (readlineSync.keyInYN('Deseja adicionar links youtube para download? ')) {
+    // 'Y' key was pressed.
+    let qtdLinks = Number(readlineSync.question('Quantos links deseja adicionar? '))
+
+    for (let l=1; l<=qtdLinks; l++) {
+        let link = readlineSync.question(`Insira a url do video ${l}? `)
+        urlVids.push(link.trim())
+        console.clear()
+    }
+    console.log('')
+    console.log('Iniciando conversão...')
+    getMP3(urlVids)
+
+  } else {
+    // Another key was pressed.
+    console.log('Searching another...');
+    // Do something...
+  }
+
+
+async function getMP3(urls) {
+
+    fs.mkdirSync('./audios')
+    
+    await urls.forEach(async vid => {
+        await downloadSong(vid, {
+            getTags: false,
+            outputDir: './audios'
+        })
+    })
+}
+
+
+
+
+
+
